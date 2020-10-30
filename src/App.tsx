c53aaec47b8ae46aa9774753b58b908ea2ceb65b
M.Shahzaib <mohdshahzaib24@gmail.com>
2020-10-30 22:39:56 +0500

    implemented pwa functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 import "./App.css";
+import firebase from './firebase';
 import QuizCard from "./Components/QuizCard/QuizCard";
 
 import { getQuiz } from "./services/quiz_service";
@@ -18,6 +20,30 @@ function App() {
       setMyQuiz(quiz);
     };
     fetchQuiz();
+
+
+    const sendNotification = async () => {
+      const messaging = firebase.messaging();
+      // messaging.requestPermission()
+      const token = await messaging.getToken();
+      const response = await axios.post(
+        'https://fcm.googleapis.com/fcm/send',
+        {
+          notification: {
+            title: "Quiz App",
+            body: "Time To Check Your IQ",
+            click_action: "",
+            icon: 'https://www.worldofmobileapps.co/wp-content/uploads/2017/10/512x512-1.png',
+          },
+          "to": token
+        },
+        { headers: { 'Content-Type': 'application/json', 'Authorization': 'key=AAAAsYLJ3Gg:APA91bHr8msGospratPn7KXTC_46hWvwKe0nY1z-i7xXNwAsH-zcsbJTBGx3hz0Eew9dPtDCbJcUWRQXvXo_hskrpF7kWZYYnJtSWL0TfhyhFPAr6EN5DZAF6PgmpcjANHizfj7oOYvw' } }
+      );
+      console.log('Response: ', response);
+    }
+
+    sendNotification()
+
   }, [showResult]);
 
   const onSubmit = (e: React.FormEvent<EventTarget>, ans: string) => {
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/messaging';
 const config = {
-    apiKey: "AIzaSyAHbGicWfGnZltGWb71s5lbL9y194gXwLs",
-    authDomain: "project-7a-4fc20.firebaseapp.com",
-    databaseURL: "https://project-7a-4fc20.firebaseio.com",
-    projectId: "project-7a-4fc20",
-    storageBucket: "project-7a-4fc20.appspot.com",
-    messagingSenderId: "153542201034",
-    appId: "1:153542201034:web:2784a01b8017cddabeae33",
-    measurementId: "G-F095BFC50D"
+    apiKey: "AIzaSyAmvUNRt3HQRlZXKA0eo0LU5ZqQl3_t64o",
+    authDomain: "fcm-cadb5.firebaseapp.com",
+    databaseURL: "https://fcm-cadb5.firebaseio.com",
+    projectId: "fcm-cadb5",
+    storageBucket: "fcm-cadb5.appspot.com",
+    messagingSenderId: "762403478632",
+    appId: "1:762403478632:web:6eb97320d64350a9956fdd",
+    measurementId: "G-8RV7EWWKW3"
 }
 
 firebase.initializeApp(config);
